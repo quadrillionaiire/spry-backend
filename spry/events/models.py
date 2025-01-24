c2f1e7a1095c799063a6b3163f5f90839104853e
quadrillionaiire <jayla.s.haskins@gmail.com>
2025-01-24 12:51:10 -0500

    implement models for events, messaging, and jobs

--- spry/events/models.py
+++ spry/events/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    date = models.DateTimeField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_events')
+    attendees = models.ManyToManyField(User, related_name='attending_events')
+
+    def __str__(self):
+        return self.title
--- spry/jobs/models.py
+++ spry/jobs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class JobPosting(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    location = models.CharField(max_length=100, null=True, blank=True)
+    posted_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='job_postings')
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
