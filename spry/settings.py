fcae6566c436d04e7a13775cfb3e37fdb193611c
quadrillionaiire <jayla.s.haskins@gmail.com>
2025-01-24 12:47:40 -0500

    created events, messaging, and jobs apps in spry

--- spry/settings.py
+++ spry/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #custom apps
+    'users',
+    'events',
+    'messaging',
+    'jobs'
 ]
 
 MIDDLEWARE = [
